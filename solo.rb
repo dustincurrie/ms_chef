d759f3068ea39a113981891446dc5e79d9e5f152
msonnabaum <msonnabaum@lucid.acquia.andover>
2010-10-27 02:51:03 -0400

    Mostly working.

--- solo.rb
+++ solo.rb
@@ -2,12 +2,11 @@
 # Chef Solo Config File
 #
 
-#log_level          :info
-#log_location       STDOUT
-#file_cache_path    "/srv/chef/cookbooks"
-file_cache_path    "/Users/msonnabaum/Dropbox/chef/chef-solo"
-cookbook_path      "/Users/msonnabaum/Dropbox/chef/chef-solo/cookbooks"
-role_path          "/Users/msonnabaum/Dropbox/chef-solo/roles"
+log_level          :info
+log_location       STDOUT
+file_cache_path    "/home/msonnabaum/ms_chef"
+cookbook_path      "/home/msonnabaum/ms_chef/cookbooks"
+role_path          "/home/msonnabaum/ms_chef/roles"
 
-#ssl_verify_mode    :verify_none
+ssl_verify_mode    :verify_none
 #Chef::Log::Formatter.show_time = false
