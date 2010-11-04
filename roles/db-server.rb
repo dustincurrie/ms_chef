5dc3f9a9af3e897b26dc5672165e7c4d7d5ff107
msonnabaum <msonnabaum@lucid.acquia.andover>
2010-11-03 21:19:58 -0400

    Initial commit of db role.

--- roles/db-server.rb
+++
@@ -1,5 +0,0 @@
-name "dbserver"
-description "The base role for data base servers"
-recipes ["mysql::client", "mysql::server"]
-#default_attributes "apache2" => { "listen_ports" => [ "8080", "443" ] }
-#override_attributes "apache2" => { "max_children" => "50" }
